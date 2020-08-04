58f2f1354a531ad826a6606bfa6ee55df6699e0a
yjw8860 <48882797+yjw8860@users.noreply.github.com>
2020-08-04 23:48:37 +0900

    addEventListner....shit...

--- js/greeting.js
+++ js/greeting.js
@@ -1,16 +1,33 @@
 const form_ = document.querySelector(".js__form"),
   input_ = document.querySelector(".js__form input"),
-  USER_LS = "currentUser";
+  USER_LS = "currentUser",
+  clock_ = document.querySelector('.js__clock'),
+  welcome_ = document.querySelector('.welcome'),
+  message_ = document.createElement('h1');
 
-function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-  } else {
+function handleInput(event) {
+  event.preventDefault();
+  const currentValue = input_.value;
+  localStorage.setItem(USER_LS, currentValue);
+  clock_.classList.add('animation');
+  clock_.classList.add('flex-column');
+  welcome_.style.display = 'none';
+  message_.classList.add('welcome');
+  message_.innerText = 'Welcome ' + `${currentValue}`
+  clock_.appendChild(message_);
+}
+
+function checkUserName(){
+  if(localStorage.getItem(USER_LS) === null){
+    clock_.style.display = 'none';
+  }else{
+    welcome_.style.display = 'none';
   }
 }
 
-function init() {
-  loadName();
+function getUserName(){
+  form_.addEventListener("submit",handleInput);
 }
 
-init();
+checkUserName();
+getUserName();
